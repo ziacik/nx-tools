f2f62b93829a397d85f4a5ebc780bd6401040fab
František Žiačik <ziacik@gmail.com>
2023-09-09 20:19:21 +0200

    feat: verify-build first implementation

--- tools/scripts/publish.mjs
+++ tools/scripts/publish.mjs
@@ -34,16 +34,10 @@ invariant(
 const graph = readCachedProjectGraph();
 const project = graph.nodes[name];
 
-invariant(
-	project,
-	`Could not find project "${name}" in the workspace. Is the project.json configured correctly?`
-);
+invariant(project, `Could not find project "${name}" in the workspace. Is the project.json configured correctly?`);
 
 const outputPath = project.data?.targets?.build?.options?.outputPath;
-invariant(
-	outputPath,
-	`Could not find "build.options.outputPath" of project "${name}". Is project.json configured  correctly?`
-);
+invariant(outputPath, `Could not find "build.options.outputPath" of project "${name}". Is project.json configured  correctly?`);
 
 process.chdir(outputPath);
 
